65f17175a144396af54f00b62352392926bf057d
WinterKwon <winterkwon7017@gmail.com>
2022-07-07 01:49:26 +0900

    포트 경로 수정

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('build'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build.index.html')
 })
 
-app.listen(3000, ()=> console.log('Server is running...'))
+app.listen(PORT, ()=> console.log('Server is running...'))
